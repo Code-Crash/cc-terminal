9f8e838dbb41df641b6369d22c89ad5402d33570
Code-Crash <pravintiwari199@gmail.com>
2019-01-11 20:50:48 +0530

    added class support and fix he bug for delay output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CcTerminalService } from '../../projects/cc-terminal/src/public_api';
 @Component({
   selector: 'cc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cc-terminal-app';
-  _externalConfig: any;
+  _classes: any;
+  _tService: CcTerminalService;
+  _command = '';
+  _config: any;
+  _prompt = { end: '$', user: 'Sumit', separator: '@', path: '\\' };
 
-  constructor() {
+  constructor(_tService: CcTerminalService) {
+    // this._tComponent._command = '';
+    // this._initializeConfig();
+    this._tService = _tService;
     // Add more config support
-    this._externalConfig = {
-      classes: {
-        section: '',
-        viewport: '',
-        input: '',
-        cursor: ''
+    this._classes = {
+      section: {
+        color: 'lightgreen', background: 'black'
+      },
+      viewport: {
+        color: 'yellow', background: 'black'
+      },
+      input: {
+        color: 'red', background: 'black'
+      },
+      cursor: {
+        color: 'green', background: 'black'
       }
     };
-
   }
+
+  ngOnInit() {
+    console.log('here');
+  }
+
+
 }
