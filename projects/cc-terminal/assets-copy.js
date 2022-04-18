d2c15a5d576d7fb28ab5553da04ef343fe4e54a5
Pravin Tiwari <pravin.tiwari@zipy.ai>
2022-04-18 18:04:11 +0530

    upgraded version

--- projects/cc-terminal/assets-copy.js
+++ projects/cc-terminal/assets-copy.js
@@ -1,10 +1,9 @@
-var wrench = require("wrench");
+var fs = require("fs-extra");
 
 var source = "./projects/cc-terminal/src/lib/assets";
 var target = "./dist/cc-terminal/lib/assets";
 
-wrench.copyDirSyncRecursive(source, target, {
-  forceDelete: true
-});
+
+fs.copy(source, target).then(() => console.log('success!')).catch(err => console.error(err))
 
 console.log("Audio Asset files successfully copied!");
